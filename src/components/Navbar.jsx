f063dd52f2b09a3f9853de8492cf1437f9936d88
Lysenko <lysenkoden@gmail.com>
2024-06-03 22:58:30 +0200

    add styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
-import { AppBar, Toolbar, Typography, styled } from '@mui/material';
-import React from 'react';
+import {
+  AppBar,
+  Box,
+  InputBase,
+  Toolbar,
+  Typography,
+  styled,
+  Badge,
+  Avatar,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+import React, { useState } from 'react';
 import EmojiNatureIcon from '@mui/icons-material/EmojiNature';
+import MailIcon from '@mui/icons-material/Mail';
+import { Notifications } from '@mui/icons-material';
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   const StyledToolbar = styled(Toolbar)({
     display: 'flex',
     justifyContent: 'space-between',
@@ -10,6 +25,27 @@ const Navbar = () => {
 
   const Search = styled('div')(({ theme }) => ({
     backgroundColor: 'white',
+    padding: '0 10px',
+    borderRadius: theme.shape.borderRadius,
+    width: '40%',
+  }));
+
+  const Icons = styled(Box)(({ theme }) => ({
+    display: 'none',
+    gap: '20px',
+    alignItems: 'center',
+    [theme.breakpoints.up('sm')]: {
+      display: 'flex',
+    },
+  }));
+
+  const UserBox = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    gap: '10px',
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+    alignItems: 'center',
   }));
 
   return (
@@ -19,8 +55,48 @@ const Navbar = () => {
           LAMA DEV
         </Typography>
         <EmojiNatureIcon sx={{ display: { xs: 'block', sm: 'none' } }} />
-        <Search>search</Search>
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <MailIcon color="action" />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ height: 30, width: 30 }}
+            src="https://pixabay.com/illustrations/man-face-portrait-male-guy-6153298/"
+            onClick={(e) => setOpen(true)}
+          />
+        </Icons>
+        <UserBox onClick={(e) => setOpen(true)}>
+          <Avatar
+            sx={{ height: 30, width: 30 }}
+            src="https://pixabay.com/illustrations/man-face-portrait-male-guy-6153298/"
+          />
+          <Typography variant="span">John</Typography>
+        </UserBox>
       </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,40 @@
-import { Box } from '@mui/material';
+import { Box, List, ListItem, ListItemButton } from '@mui/material';
 import React from 'react';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import HomeIcon from '@mui/icons-material/Home';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import { Storefront } from '@mui/icons-material';
 
 const Sidebar = () => {
   return (
-    <Box
-      backgroundColor="skyblue"
-      flex={1}
-      p={2}
-      sx={{ display: { xs: 'none', sm: 'block' } }}
-    >
-      Sidebar
+    <Box flex={1} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Homepage" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <Storefront />
+            </ListItemIcon>
+            <ListItemText primary="Marketplace" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <AccountBoxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
